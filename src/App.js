fe148e7b6e797c6a7da847d75d803b3d40761a3c
devikaupendran <devikaupendranr@gmail.com>
2024-11-06 10:21:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState , useEffect } from "react";
+import randomcolor from "randomcolor";
+import randomColor from "randomcolor";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  //setting state for count
+  let [count, setCounter] = useState(0);
+
+  //setting state for color
+  let [color, setColor] = useState("Blue")
+
+  //defining the increment button function
+  function incrementBtnHandler() {
+    setCounter((prevState) => {
+      return prevState + 1;
+    })
+    setColor(randomColor())
+  }
+
+  //defining decrement btn function
+  function decrementBtnHandler() {
+    console.log("decrement");
+    setCounter((prevState) => {
+      return prevState - 1;
+    })
+    setColor(randomColor())
+  }
+
+  useEffect(() => {
+    console.log("Hey iam use Effecy");
+    setColor(randomcolor())
+  },[count])
+
+  //defining decrement btn function
+  function resetButton() {
+    setCounter(0)
+    setColor("black")
+  }
+  return (
+    <div className="container">
+      <h1 style={{ color }}> {count} </h1>
+      <div className="btn-container">
+        <button onClick={incrementBtnHandler}>Increment</button>
+        <button onClick={resetButton}>Reset</button>
+        <button onClick={decrementBtnHandler}>Decrement</button>
+      </div>
+    </div>
+  )
+}
 export default App;
